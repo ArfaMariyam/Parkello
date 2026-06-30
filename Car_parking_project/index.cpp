e7173a519bf0307b305e0deb1a1abdd8005682f5
ArfaMariyam <arfa.mariyam.liaqat@gmail.com>
2026-06-29 20:54:35 -0700

    Add basic menu UI and executable

--- Car_parking_project/index.cpp
+++ Car_parking_project/index.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<fstream>
+#include<string.h>
+#include<process.h>
+#include<stdio.h>
+#include<conio.h>
 using namespace std;
+void menu()
+{
+cout<<endl;
+cout<<"                                        ";
+cout<<endl;
+cout<<"\t\t\t====================================================================";
+cout<<endl;
+cout<<"\t\t\t====================================================================";
+cout<<endl;
+cout<<"\n\t\t\t  1. Arrival of a Bike\t\t\t  6. Departure of Bike";
+cout<<endl;
+cout<<"\n\t\t\t  1. Arrival of a Car\t\t\t  6. Departure of Car";
+cout<<endl;
+
+}
 
 int main()
 {
-    
+    cout<<"CAR PARKING SYSTEM!!!\t!";
+    menu();
+    return 0;
 }
