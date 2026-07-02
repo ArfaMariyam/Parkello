06d653e8ac5d304f7a1c76ed185651f8e754eb67
ArfaMariyam <arfa.mariyam.liaqat@gmail.com>
2026-07-02 18:11:52 +0500

    Implement continuous menu loop with all options

--- Car_parking_project/index.cpp
+++ Car_parking_project/index.cpp
@@ -7,66 +7,100 @@
 using namespace std;
 void menu()
 {
-    cout << "!!!-----------------------------START OF MENU HERE-----------------------------!!!";
-    cout << endl;
-    cout << "                                        ";
-    cout<<endl;
-    cout << "\t\t\t====================================================================";
-    cout << endl;
-    cout << "\t\t\t====================================================================";
-    cout << endl;
-    cout << "\n\t\t\t  1. Arrival of a Bike\t\t\t  6. Departure of Bike";
-    cout << endl;
-    cout << "\n\t\t\t  2. Arrival of a Car\t\t\t  7. Departure of Car";
-    cout << endl;
-    cout << endl;
-    cout << "\n\t\t\t  3. Arrival of a Bus\t\t\t  8. Departure of Bus";
-    cout << endl;
-    cout << endl;
-    cout << "\n\t\t\t  4. Arrival of a Rishkaw\t\t\t  9. Departure of Rishkaw";
-    cout << endl;
-    cout << endl;
-    cout << "\n\t\t\t  5. Arrival of a Bicycle\t\t\t  10. Departure of Bicycle";
-    cout << endl;
-    cout << endl;
-    cout << "\t\t\t\t\t      ========================\n";
-    cout << "\t\t\t\t\t      ========================";
-    cout << endl;
-    cout << "\n\t\t\t\t\t\t11. Show the record";
-    cout << endl;
-    cout << "\n\t\t\t\t\t\t12. Edit the record";
-    cout << endl;
-    cout << "\n\t\t\t\t\t\t13. Delete the record";
-    cout << endl;
-    cout << "\n\t\t\t\t\t\t14. Exit Program";
-    cout << endl;
-    cout << "\t\t\t=================================================";
-    cout << endl;
-    cout << endl;
-    cout << "-----------------------------END OF MENU HERE-----------------------------";
+    cout <<"\n\t\t\t====================================================================";
+    cout <<"\n\t\t\t====================================================================";
+    cout <<"\n\n\t\t\t  1. Arrival of a Bike\t\t\t  6. Departure of Bike";
+    cout <<"\n\n\t\t\t  2. Arrival of a Car\t\t\t  7. Departure of Car";
+    cout <<"\n\n\t\t\t  3. Arrival of a Bus\t\t\t  8. Departure of Bus";
+    cout <<"\n\n\t\t\t  4. Arrival of a Rishkaw\t\t  9. Departure of Rishkaw";
+    cout <<"\n\n\t\t\t  5. Arrival of a Bicycle\t\t  10. Departure of Bicycle";
+    cout <<"\n\n\t\t\t\t\t      ========================";
+    cout <<"\n\n\t\t\t\t\t      ========================";
+    cout <<"\n\n\t\t\t\t\t\t11. Show the record";
+    cout <<"\n\n\t\t\t\t\t\t12. Edit the record";
+    cout <<"\n\n\t\t\t\t\t\t13. Delete the record";
+    cout <<"\n\n\t\t\t\t\t\t14. Exit Program";
+    cout << "\n\n\t\t\t===================================================================="<<endl; 
 }
 
 int main()
 {
-    cout << "CAR PARKING SYSTEM!!!";
-    int select;
-    cout<<"Enter 1 to show menu.";
-    cin>>select;
-    if(select==1)
+    int choice;
+
+    int totalAmount = 0;
+    int totalVahicles = 0;
+    int bikeCount = 0;
+    int carCount = 0;
+
+    while (true)
     {
         menu();
-        int choice;
-        cout<<"Enter your choice from menu: ";
-        cin>>choice;
-        if(choice==1)
+
+        cout << "\n\n\t\t\t Enter your choice from menu: ";
+        cin >> choice;
+
+        if (choice == 1)
         {
-            cout<<"Bike arrive choosen"<<endl;
+            cout<<"\n\t\tBike arrive choosen"<<endl;
         }
-        else if(choice==2)
+        else if (choice == 2)
         {
-            cout<<"Car arrive choosen"<<endl;
+            cout << "\n\t\tCar arrive choosen" << endl;
         }
-        
+        else if (choice == 3)
+        {
+            cout << "\n\t\tBus arrive choosen" << endl;
+        }
+        else if (choice == 4)
+        {
+            cout << "\n\t\tRishkaw arrive chosen" << endl;
+        }
+        else if (choice == 5)
+        {
+            cout << "\n\t\tBicycle is arrived" << endl;
+        }
+        else if (choice == 6)
+        {
+            cout << "\n\t\tBike is departure" << endl;
+        }
+        else if (choice == 7)
+        {
+            cout << "\n\t\tCar departure" << endl;
+        }
+        else if (choice == 8)
+        {
+            cout << "\n\t\tBus departure" << endl;
+        }
+        else if (choice == 9)
+        {
+            cout << "\n\t\tRishkaw departure" << endl;
+        }
+        else if (choice == 10)
+        {
+            cout << "\n\t\tBicyce departure" << endl;
+        }
+        else if (choice == 11)
+        {
+            cout << "\n\t\tShow record!!!" << endl;
+        }
+        else if (choice == 12)
+        {
+            cout << "\n\t\tEdit record!!!" << endl;
+        }
+        else if (choice == 13)
+        {
+            cout << "\n\t\tDelete record!!!" << endl;
+        }
+        else if (choice == 14)
+        {
+            cout << "\n\t\tEXIT PROGRAM!!" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "\n\t\tChose from given options only" << endl;
+        }
+        cout<<"\n\t\t\t===================================================================="<<endl;
     }
     return 0;
 }
