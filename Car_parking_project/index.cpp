625da4efbd921b4c35f814bf17353d1b4150e432
ArfaMariyam <arfa.mariyam.liaqat@gmail.com>
2026-07-05 13:26:09 +0500

    Initial commit: Parkello Car Parking Engine

---
+++ Car_parking_project/index.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <fstream>
+// #include<string.h>
+// #include<process.h>
+// #include<stdio.h>
+// #include<conio.h>
+using namespace std;
+
+void menu()
+{
+    cout << "\n\t\t\t=====================================================================";
+    cout << "\n\t\t\t                         WELCOME TO PARKELLO";
+    cout << "\n\t\t\t=====================================================================";
+    cout << "\n\n\t\t\t  1. Arrival of a Bike\t\t\t  6. Departure of Bike";
+    cout << "\n\n\t\t\t  2. Arrival of a Car\t\t\t  7. Departure of Car";
+    cout << "\n\n\t\t\t  3. Arrival of a Bus\t\t\t  8. Departure of Bus";
+    cout << "\n\n\t\t\t  4. Arrival of a Rishkaw\t\t  9. Departure of Rishkaw";
+    cout << "\n\n\t\t\t  5. Arrival of a Bicycle\t\t  10. Departure of Bicycle";
+    cout << "\n\n\t\t\t\t\t      ========================";
+    cout << "\n\n\t\t\t\t\t      ========================";
+    cout << "\n\n\t\t\t\t\t\t11. Show the record";
+    cout << "\n\n\t\t\t\t\t\t12. Edit the record";
+    cout << "\n\n\t\t\t\t\t\t13. Delete the record";
+    cout << "\n\n\t\t\t\t\t\t14. Exit Program";
+    cout << "\n\n\t\t\t====================================================================" << endl;
+}
+
+int main()
+{
+    int choice;
+
+    int totalAmount = 0;
+    int totalVahicles = 0;
+
+    int bikeCount = 0;
+    int carCount = 0;
+    int busCount = 0;
+    int rishkawCount = 0;
+    int bicycleCount = 0;
+
+    while (true)
+    {
+        menu();
+
+        cout << "\n\n\t\t\t Enter your choice from menu: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            cout << "\n\t\tBike arrived" << endl;
+            bikeCount = bikeCount + 1;
+            totalAmount = totalAmount + 20;
+        }
+        else if (choice == 2)
+        {
+            cout << "\n\t\tCar arrived" << endl;
+            carCount = carCount + 1;
+            totalAmount = totalAmount + 30;
+        }
+        else if (choice == 3)
+        {
+            cout << "\n\t\tBus arrived" << endl;
+            busCount = busCount + 1;
+            totalAmount = totalAmount + 50;
+        }
+        else if (choice == 4)
+        {
+            cout << "\n\t\tRishkaw arrived" << endl;
+            rishkawCount = rishkawCount + 1;
+            totalAmount = totalAmount + 30;
+        }
+        else if (choice == 5)
+        {
+            cout << "\n\t\tBicycle arrived" << endl;
+            bicycleCount = bicycleCount + 1;
+            totalAmount = totalAmount + 10;
+        }
+        else if (choice == 6)
+        {
+            cout << "\n\t\tBike departure" << endl;
+            bikeCount = bikeCount - 1;
+            totalAmount = totalAmount - 20;
+        }
+        else if (choice == 7)
+        {
+            cout << "\n\t\tCar departure" << endl;
+            carCount = carCount - 1;
+            totalAmount = totalAmount - 30;
+        }
+        else if (choice == 8)
+        {
+            cout << "\n\t\tBus departure" << endl;
+            busCount = busCount - 1;
+            totalAmount = totalAmount - 50;
+        }
+        else if (choice == 9)
+        {
+            cout << "\n\t\tRishkaw departure" << endl;
+            rishkawCount = rishkawCount - 1;
+            totalAmount = totalAmount - 30;
+        }
+        else if (choice == 10)
+        {
+            cout << "\n\t\tBicyce departure" << endl;
+            bicycleCount = bicycleCount - 1;
+            totalAmount = totalAmount - 10;
+        }
+        else if (choice == 11)
+        {
+            cout << "\n\t\tShow record!!!" << endl;
+            cout << "Record:" << endl;
+            cout << "The current number of Cars parked is:" << carCount << endl;
+            cout << "The current number of Bikes parked is:" << bikeCount << endl;
+            cout << "The current number of Buses parked is:" << busCount << endl;
+            cout << "The current number of Bicycles parked is:" << bicycleCount << endl;
+            cout << "Total current cash is:" << totalAmount << endl;
+        }
+        else if (choice == 12)
+        {
+            cout << "\n\t\tEdit record!!!" << endl;
+            
+            int edit = 0;
+            
+            cout << "Select what do you want to edit:";
+            cout<<"\n\n\t\t\t  1. Cars \t\t\t  2. Bikes ";
+            cout<<"\n\n\t\t\t  3. Buses \t\t\t  4. Rishkaws ";
+            cout<<"\n\n\t\t\t  5. Bicycles \t\t\t  6. EXIT ";
+            cin>>edit;
+            if(edit == 1)
+            {
+                cout<<"Editing Cars"<<endl;
+            }
+            else if(edit == 2)
+            {
+                cout<<"Editing Bikes"<<endl;
+            }
+            else if(edit == 3)
+            {
+                cout<<"Editing Buses"<<endl;
+            }
+            else if(edit == 4)
+            {
+                cout<<"Editing Rishkaws"<<endl;
+            }
+            else if(edit == 5)
+            {
+                cout<<"Editing Bicycles"<<endl;
+            }
+            else if(edit == 6)
+            {
+                cout<<"Exiting Editing"<<endl;
+            }
+            else
+            {
+                cout<<"Please only select from given options"<<endl;
+            }
+        }
+        else if (choice == 13)
+        {
+            cout << "\n\t\tDelete record!!!" << endl;
+            cout << "are you sure?";
+        }
+        else if (choice == 14)
+        {
+            cout << "\n\t\tEXIT PROGRAM!!!" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "\n\t\tInvalid choice - Chose from given options only!!!" << endl;
+        }
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Car Parking Project
+This is a terminal app designed to help the parking operator by managing the arrival and departure of vehicles, maintaining the log history, and calculating fees.
+
+### How I made it:
+I am combining all the foundational concepts I've been teaching myself:
+* The use of **cout** to print the menu. Tap [--> here <--](demo/ScreenShorts/cout(menu).png) to see code screenshot.
+* **cin** to get user input. Tap [--> here <--](demo/ScreenShorts/cin(user_Input).png) to see code screenshot.
+* **while(true) loop** to display the menu again and again. Tap [--> here <--](demo/ScreenShorts/loop.png) to see code screenshot.
+* **if-else statements** to respond to user input. Tap [--> here <--](demo/ScreenShorts/loop.png) to see code screenshot.
+
+### Project Roadmap & Phases:
+I divided the project into 5 phases/milestones:
+* **Phase 1 (Done):** The interactive menu and options navigation are working completely fine!
+* **Phase 2 (Current):** I am currently implementing the mathematical calculations via using the variables like `totalAmount`, `totalVehicles`, `bikeCount`, and `carCount` and actively modifying them to track arrivals and departures in real-time.
+* **Phase 3:** The 3rd phase will be about adding the authentication mechanism for the operator to login using username and password.
+* **Phase 4:** The 4th phase will be about adding the functionality of file management to securely save and load the logs directly from local text files.
+
+### Live Demo Loop
+Here is a preview of the interactive workspace layout running natively:
+
+![Project Demo](demo/demo.gif)
+
+# How to run and test
+I compiled a ready-to-run `index.exe` file which anyone can download by clicking [--> here <--](https://github.com/ArfaMariyam/Programming-fundamentals-in-CPP/releases) and just double-click it to run and test.
