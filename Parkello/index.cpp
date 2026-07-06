628ffc8f2a142cc61368379ac07a8f138385e0c6
ArfaMariyam <arfa.mariyam.liaqat@gmail.com>
2026-07-06 18:21:02 +0500

    Guard arrivals/departures, improve reporting

--- Parkello/index.cpp
+++ Parkello/index.cpp
@@ -29,99 +29,152 @@ int main()
 {
     int choice; // main menu choice
 
-    int totalAmount = 0; // total cash
+    int totalAmount = 0;   // total cash
     int totalVahicles = 0; // total number of Vahicles
 
     // Vahicles counts
-    int bikeCount = 0; // bike fee 20
-    int carCount = 0; // car fee 50
-    int busCount = 0; // bus fee 100
+    int bikeCount = 0;    // bike fee 20
+    int carCount = 0;     // car fee 50
+    int busCount = 0;     // bus fee 100
     int rishkawCount = 0; // rishkaw fee 50
     int bicycleCount = 0; // bicycle fee 10
 
-    while (true)
+    while (true) // main loop
     {
         menu();
 
         cout << "\n\n\t\t\t Enter your choice from menu: ";
         cin >> choice;
 
+        // ==========Arrivals==========
         if (choice == 1)
         {
             cout << "\n\t\tBike arrived" << endl;
             bikeCount = bikeCount + 1;
+            totalVahicles = totalAmount + 1;
             totalAmount = totalAmount + 20;
         }
         else if (choice == 2)
         {
             cout << "\n\t\tCar arrived" << endl;
             carCount = carCount + 1;
+            totalVahicles = totalVahicles + 1;
             totalAmount = totalAmount + 50;
         }
         else if (choice == 3)
         {
             cout << "\n\t\tBus arrived" << endl;
             busCount = busCount + 1;
+            totalVahicles = totalVahicles + 1;
             totalAmount = totalAmount + 100;
         }
         else if (choice == 4)
         {
             cout << "\n\t\tRishkaw arrived" << endl;
             rishkawCount = rishkawCount + 1;
+            totalVahicles = totalVahicles + 1;
             totalAmount = totalAmount + 50;
         }
         else if (choice == 5)
         {
             cout << "\n\t\tBicycle arrived" << endl;
             bicycleCount = bicycleCount + 1;
+            totalVahicles = totalVahicles + 1;
             totalAmount = totalAmount + 10;
         }
+
+        //==========Departures==========
+
         else if (choice == 6)
         {
-            cout << "\n\t\tBike departure" << endl;
-            bikeCount = bikeCount - 1;
-            totalAmount = totalAmount - 20;
+            if (bikeCount > 0)
+            {
+                cout << "\n\t\tBike departure" << endl;
+                bikeCount = bikeCount - 1;
+                totalVahicles = totalVahicles - 1;
+                totalAmount = totalAmount - 20;
+            }
+            else
+            {
+                cout << "Error: No Bikes currently parked. " << endl;
+            }
         }
         else if (choice == 7)
         {
-            cout << "\n\t\tCar departure" << endl;
-            carCount = carCount - 1;
-            totalAmount = totalAmount - 50;
+            if (carCount > 0)
+            {
+                cout << "\n\t\tCar departure" << endl;
+                carCount = carCount - 1;
+                totalVahicles = totalVahicles - 1;
+                totalAmount = totalAmount - 50;
+            }
+            else
+            {
+                cout << "Error: No Cars currently parked. " << endl;
+            }
         }
         else if (choice == 8)
         {
-            cout << "\n\t\tBus departure" << endl;
-            busCount = busCount - 1;
-            totalAmount = totalAmount - 100;
+            if (busCount > 0)
+            {
+                cout << "\n\t\tBus departure" << endl;
+                busCount = busCount - 1;
+                totalVahicles = totalVahicles - 1;
+                totalAmount = totalAmount - 100;
+            }
+            else
+            {
+                cout << "\n\t\tError: No Buses currently parked. " << endl;
+            }
         }
         else if (choice == 9)
         {
-            cout << "\n\t\tRishkaw departure" << endl;
-            rishkawCount = rishkawCount - 1;
-            totalAmount = totalAmount - 50;
+            if (rishkawCount > 0)
+            {
+                cout << "\n\t\tRishkaw departure" << endl;
+                rishkawCount = rishkawCount - 1;
+                totalAmount = totalAmount - 50;
+            }
+            else
+            {
+                cout << "Error: No rishkaws curretly parked. " << endl;
+            }
         }
         else if (choice == 10)
         {
-            cout << "\n\t\tBicyce departure" << endl;
-            bicycleCount = bicycleCount - 1;
-            totalAmount = totalAmount - 10;
+            if (bicycleCount > 0)
+            {
+                cout << "\n\t\tBicyce departure" << endl;
+                bicycleCount = bicycleCount - 1;
+                totalAmount = totalAmount - 10;
+            }
+            else
+            {
+                cout << "Error: No Bicycles currently parked. " << endl;
+            }
         }
+
+        //========Show Record============
         else if (choice == 11)
         {
-            cout << "===========================" << endl;
-            cout << "===========================" << endl;
+            cout << "\n\t\t===========================" << endl;
+            cout << "\n\t\t===========================" << endl;
             cout << "\n\t\tShowing record!!!" << endl;
-            cout << "===========================" << endl;
-            cout << "===========================" << endl;
+            cout << "\n\t\t===========================" << endl;
+            cout << "\n\t\t===========================" << endl;
             cout << "\n\t\tRecord:" << endl;
-            cout << "=====================================================" << endl;
+            cout << "\n\t\t=====================================================" << endl;
             cout << "\n\t\tThe current number of Cars parked is:" << carCount << endl;
             cout << "\n\t\tThe current number of Bikes parked is:" << bikeCount << endl;
             cout << "\n\t\tThe current number of Buses parked is:" << busCount << endl;
             cout << "\n\t\tThe current number of Bicycles parked is:" << bicycleCount << endl;
-            cout << "\n\t\tTotal current cash is:" << totalAmount << endl;
-            cout << "=====================================================" << endl;
+            cout << "\n\t\t-------------------------------------------------------" << endl;
+            cout << "\n\t\tTotal current Vahicles parked is:" << totalVahicles << endl;
+            cout << "\n\t\tTotal current cash ledger is:" << totalAmount << endl;
+            cout << "\n\t\t=====================================================" << endl;
         }
+
+        //===========Editing=============
         else if (choice == 12)
         {
             cout << "\n\t\tEdit record!!!" << endl;
@@ -132,6 +185,8 @@ int main()
             cout << "\n\n\t\t\t  1. Cars \t\t\t  2. Bikes ";
             cout << "\n\n\t\t\t  3. Buses \t\t\t  4. Rishkaws ";
             cout << "\n\n\t\t\t  5. Bicycles \t\t\t  6. EXIT ";
+            cout<<"";
+            cout<<"\n\n\t\t\t Enter option:"<<endl;
             cin >> edit;
             if (edit == 1)
             {
